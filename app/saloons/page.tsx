45c9de7657a0536154805e89d9aba33c4745261b
Toqeershah <toqeerabi85@gmail.com>
2024-09-19 15:12:22 +0500

    displaying the saloons

--- app/saloons/page.tsx
+++ app/saloons/page.tsx
@@ -1,13 +1,40 @@
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+import prisma from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
 
-const SaloonsPage = () => {
+const SaloonsPage = async () => {
+  const saloons = await prisma.saloon.findMany({})
   return (
     <div>
-      <Button><Link href='/saloons/new'>New Saloon</Link></Button>
+      <div className="mb-5">
+      <Button>
+        <Link href="/saloons/new">New Saloon</Link>
+      </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Saloon</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">Created At</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {saloons.map(saloon => (
+            <Table.Row key={saloon.id}>
+              <Table.Cell>
+                {saloon.title}
+                <div className="block md:hidden">{saloon.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">{saloon.status}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">{saloon.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export default SaloonsPage
+export default SaloonsPage;
