81207b171aa6495650418e016e78b9e493a89ba2
Toqeershah <toqeerabi85@gmail.com>
2024-09-20 00:30:14 +0500

    styling single saloon detail page

--- app/saloons/[id]/page.tsx
+++ app/saloons/[id]/page.tsx
@@ -1,4 +1,6 @@
+import SaloonStatusBadge from "@/app/components/SaloonStatusBadge";
 import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -7,22 +9,25 @@ interface Props {
 }
 
 const SaloonDetailPage = async ({ params }: Props) => {
-
   // if (typeof params.id !== 'number') notFound();
 
-  const singleSaloon = await prisma.saloon.findUnique({
+  const saloon = await prisma.saloon.findUnique({
     where: { id: parseInt(params.id) },
   });
 
-  if (!singleSaloon) notFound();
+  if (!saloon) notFound();
 
   return (
     <div>
-      <h2 className="font-bold text-lg">Saloon Detail Page</h2>
-      <p>{singleSaloon.title}</p>
-      <p>{singleSaloon.description}</p>
-      <p>{singleSaloon.status}</p>
-      <p>{singleSaloon.createdAt.toDateString()}</p>
+      <h2 className="mb-5 font-semi-bold text-lg">Single Saloon Detail Page</h2>
+      <Heading as="h2">{saloon.title}</Heading>
+      <Flex className="space-x-4" my='4'>
+        <SaloonStatusBadge status={saloon.status} />
+        <Text>{saloon.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+       <p>{saloon.description}</p>
+      </Card>
     </div>
   );
 };
