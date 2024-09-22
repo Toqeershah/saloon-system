3852c98b039a7ba473041010094fa3a567fa0948
Toqeershah <toqeerabi85@gmail.com>
2024-09-22 18:52:57 +0500

    added connfirmation delete dialog

--- app/saloons/[id]/DeleteSaloonButton.tsx
+++ app/saloons/[id]/DeleteSaloonButton.tsx
@@ -1,12 +1,33 @@
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import React from "react";
 
-const DeleteSaloonButton = ({saloonId}: {saloonId: number}) => {
+const DeleteSaloonButton = ({ saloonId }: { saloonId: number }) => {
   return (
     <div>
-        <Button color='red'>Delete Saloon</Button>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Saloon</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>
+            Delete this Saloon
+          </AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you Sure you want to delete Saloon? This action cannot be undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap='4'>
+            <AlertDialog.Cancel>
+                <Button variant="surface" color="gray">Cancel</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+                <Button color="red">Delete Saloon</Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
     </div>
-  )
-}
+  );
+};
 
-export default DeleteSaloonButton
+export default DeleteSaloonButton;
