f5ccbb0cd4cb75a478d855d3f6ecf52e6ef44efd
Toqeershah <toqeerabi85@gmail.com>
2024-09-24 18:01:05 +0500

    added filter saloons func with searchParams/queryParams

--- app/saloons/list/page.tsx
+++ app/saloons/list/page.tsx
@@ -1,14 +1,29 @@
 //fetching/rendering all saloons table(page)
 
 import prisma from "@/prisma/client";
-import {  Table } from "@radix-ui/themes";
-import {SaloonStatusBadge, Link} from '@/app/components'
+import { Table } from "@radix-ui/themes";
+import { SaloonStatusBadge, Link } from "@/app/components";
 import delay from "delay";
 import SaloonActions from "./SaloonActions";
+import { Status } from "@prisma/client";
 
-const SaloonsPage = async () => {
-  const saloons = await prisma.saloon.findMany({});
-  await delay(1000); // simulate network latency
+interface Props {
+  searchParams: { status: Status };
+}
+
+const SaloonsPage = async ({
+  searchParams,
+}: Props) => {
+  // console.log(searchParams.status);
+  const statuses = Object.values(Status)
+  console.log(statuses)
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+
+
+  const saloons = await prisma.saloon.findMany({
+    where: { status },
+  });
+  await delay(1000);
 
   return (
     <div>
@@ -17,17 +32,19 @@ const SaloonsPage = async () => {
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Saloon</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Created At</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created At
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {saloons.map(saloon => (
+          {saloons.map((saloon) => (
             <Table.Row key={saloon.id}>
               <Table.Cell>
-                <Link href={`/saloons/${saloon.id}`}>
-                 {saloon.title}
-                </Link>
+                <Link href={`/saloons/${saloon.id}`}>{saloon.title}</Link>
                 <div className="block md:hidden">
                   <SaloonStatusBadge status={saloon.status} />
                 </div>
@@ -35,7 +52,9 @@ const SaloonsPage = async () => {
               <Table.Cell className="hidden md:table-cell">
                 <SaloonStatusBadge status={saloon.status} />
               </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">{saloon.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {saloon.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
