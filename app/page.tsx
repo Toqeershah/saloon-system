6b7e9b25f84b1032d7cd21b050091674d89f551b
Toqeershah <toqeerabi85@gmail.com>
2024-09-24 23:25:57 +0500

    added pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    // <h1>Hello</h1>
+    <Pagination itemCount={100} pageSize={10} currentPage={10} />
   );
 }
