9d5672c4ed0e28ddc5587c003d5033d29518a0cc
Toqeershah <toqeerabi85@gmail.com>
2024-09-25 17:09:03 +0500

    rendering 3 components on dashboard

--- app/SaloonChart.tsx
+++ app/SaloonChart.tsx
@@ -1,31 +1,31 @@
-'use client'
-import { Card } from '@radix-ui/themes';
-import {ResponsiveContainer, BarChart, XAxis, YAxis, Bar} from 'recharts'
-import React from 'react'
+"use client";
+import { Card } from "@radix-ui/themes";
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+import React from "react";
 
 interface Props {
-    open: number;
-    shortBreak: number;
-    closed: number;
-  }
-
-const SaloonChart = ({open, shortBreak, closed}: Props) => {
-    const data = [
-        {label: 'Open', value: open},
-        {label: 'Short Break', value: shortBreak},
-        {label: 'Closed', value: closed},
-    ]
-  return (
-    <Card>
-        <ResponsiveContainer width='100%' height={300}>
-            <BarChart data={data}>
-                <XAxis dataKey='label' />
-                <YAxis />
-                <Bar dataKey='value' fill='#8884d8' barSize={90} />
-            </BarChart>
-        </ResponsiveContainer>
-    </Card>
-  )
+  open: number;
+  shortBreak: number;
+  closed: number;
 }
 
-export default SaloonChart
+const SaloonChart = ({ open, shortBreak, closed }: Props) => {
+  const data = [
+    { label: "Open", value: open },
+    { label: "Short Break", value: shortBreak },
+    { label: "Closed", value: closed },
+  ];
+  return (
+    <Card>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <XAxis dataKey="label" />
+          <YAxis />
+          <Bar dataKey="value" fill="#8884d8" barSize={70} />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default SaloonChart;
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,22 @@ import prisma from "@/prisma/client";
 import LatestSaloons from "./LatestSaloons";
 import SaloonSummary from "./SaloonSummary";
 import SaloonChart from "./SaloonChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home() {
-
-  const open = await prisma.saloon.count({where: {status: 'OPEN'}})
-  const closed = await prisma.saloon.count({where: {status: 'CLOSED'}})
-  const shortBreak = await prisma.saloon.count({where: {status: 'SHORT_BREAK'}})
+  const open = await prisma.saloon.count({ where: { status: "OPEN" } });
+  const closed = await prisma.saloon.count({ where: { status: "CLOSED" } });
+  const shortBreak = await prisma.saloon.count({
+    where: { status: "SHORT_BREAK" },
+  });
 
   return (
-    <SaloonChart open={open} closed={closed} shortBreak={shortBreak} />
-    // <SaloonSummary open={open} closed={closed} shortBreak={shortBreak} />
-    // <LatestSaloons />
+    <Grid columns={{ initial: "1", md: "2" }} gap='5'>
+      <Flex direction='column' gap='5'>
+        <SaloonSummary open={open} closed={closed} shortBreak={shortBreak} />
+        <SaloonChart open={open} closed={closed} shortBreak={shortBreak} />
+      </Flex>
+      <LatestSaloons />
+    </Grid>
   );
 }
