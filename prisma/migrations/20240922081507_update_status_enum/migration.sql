08657de37504d2b3a37272f662916ed95d45420c
Toqeershah <toqeerabi85@gmail.com>
2024-09-26 11:33:34 +0500

    configured migrations to initial and updated scripts

--- prisma/migrations/20240922081507_update_status_enum/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [IN_PROGRESS] on the enum `Saloon_status` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `saloon` MODIFY `status` ENUM('OPEN', 'CLOSED', 'SHORT_BREAK') NOT NULL DEFAULT 'OPEN';
